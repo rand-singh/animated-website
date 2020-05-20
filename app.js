47f970a18c2b01b49acd86abc74568157e30c4b9
Randeep Singh <randeeprattan90@gmail.com>
2020-05-20 21:59:36 +0100

    Added page transitions

--- app.js
+++ app.js
@@ -128,6 +128,12 @@ barba.init({
       namespace: "tech",
       beforeEnter() {
         logo.href = "../index.html";
+        gsap.fromTo(
+          ".nav-header",
+          1,
+          { y: "100%" },
+          { y: "0%", ease: "power2.inOut" }
+        );
       },
     },
   ],
@@ -136,11 +142,13 @@ barba.init({
       leave({ current, next }) {
         let done = this.async();
         const tl = gsap.timeline({ default: { ease: "power2.inOut" } });
+        tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
         tl.fromTo(
-          current.container,
-          1,
-          { opacity: 1 },
-          { opacity: 0, onComplete: done }
+          ".swipe",
+          0.75,
+          { x: "-100%" },
+          { x: "0%", onComplete: done },
+          "-=0.5"
         );
       },
       enter({ current, next }) {
@@ -149,11 +157,13 @@ barba.init({
         window.scrollTo(0, 0);
         const tl = gsap.timeline({ default: { ease: "power2.inOut" } });
         tl.fromTo(
-          next.container,
-          1,
-          { opacity: 0 },
-          { opacity: 1, onComplete: done }
+          ".swipe",
+          0.75,
+          { x: "-0%" },
+          { x: "100%", stagger: 0.25, onComplete: done },
+          "-=0.5"
         );
+        tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
       },
     },
   ],
