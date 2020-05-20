f30ec3fdfafb47942e5d7cc310664d4b55aea203
Randeep Singh <randeeprattan90@gmail.com>
2020-05-20 20:46:19 +0100

    toggle events for burger menu

--- app.js
+++ app.js
@@ -62,8 +62,9 @@ function animateSlides() {
   });
 }
 
-let mouse = document.querySelector(".cursor");
-let mouseTxt = mouse.querySelector("span");
+const mouse = document.querySelector(".cursor"),
+  mouseTxt = mouse.querySelector("span"),
+  burger = document.querySelector(".burger");
 
 function cursor(e) {
   mouse.style.top = e.pageY + "px";
@@ -89,6 +90,26 @@ function activeCursor(e) {
   }
 }
 
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45deg", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45deg", y: -5, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%" });
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "-0", y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%" });
+    document.body.classList.remove("hide");
+  }
+}
+
+// event listeners
+burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
 animateSlides();
